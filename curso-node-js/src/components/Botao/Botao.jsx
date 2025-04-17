c5d9637b07db745c50f0c9a2421888895059e98e
Karinne Ventura <125403596+Kakventura@users.noreply.github.com>
2025-04-17 12:56:05 -0300

    Módulo 3 - React

--- curso-node-js/src/components/Botao/Botao.jsx
+++ curso-node-js/src/components/Botao/Botao.jsx
@@ -1,9 +1,10 @@
-//import style from './Botao.module.css';
+import {TIPOS_BOTAO} from './constantes';
+import style from './Botao.module.css';
 
 const Botao = (props) => {
-    const {texto, tipo = "primario", ...OutrasProps} = props;
+    const {texto, tipo = TIPOS_BOTAO.PRIMARIO, ...OutrasProps} = props;
     return(
-        <button tipo={tipo} {...OutrasProps}>
+        <button className={style.Botao} tipo={tipo} {...OutrasProps}>
            {texto} 
         </button>
     )
